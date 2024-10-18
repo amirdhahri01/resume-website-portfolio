ff8a0eb16aa38319bfbeec4f750a29b97151ad73
Amir Dhahri <amirdhahri01@gmail.com>
2024-10-18 22:05:03 +0100

    The hom component structure is done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,36 @@
-import React from "react";
+import { Component } from "react";
+import Navbar from "./navbar/Navbar";
+import Profile from "./pages/Profile/Profile";
+import Skills from "./pages/skills/Skills";
+import Experiences from "./pages/experiences/Experiences";
+import Educations from "./pages/educations/Educations";
+import Portfolios from "./pages/portfolios/Portfolios";
+import About from "./pages/About/About";
 
-const Home = () => {
-  return <div>Home</div>;
-};
+class Home extends Component {
+  render() {
+    return (
+      <>
+        <section>
+          <Navbar />
+          <div className="container">
+            <div className="row">
+              <div className="col s12 m3">
+                <Profile />
+              </div>
+              <div className="col s12 m9">
+                <About />
+                <Skills />
+                <Experiences />
+                <Educations />
+                <Portfolios />
+              </div>
+            </div>
+          </div>
+        </section>
+      </>
+    );
+  }
+}
 
 export default Home;
