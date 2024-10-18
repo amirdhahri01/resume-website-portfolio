9eabbafdc2cff47b98f90a287e0e7cfbf91cbdf3
Amir Dhahri <amirdhahri01@gmail.com>
2024-10-18 21:47:02 +0100

    The creation of the react routers is done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import "./App.css";
-
+import "materialize-css/dist/css/materialize.min.css";
+import "materialize-css/dist/css/materialize.min.js";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Skills from "./components/pages/skills/Skills";
+import Experiences from "./components/pages/experiences/Experiences";
+import Educations from "./components/pages/educations/Educations";
+import Portfolios from "./components/pages/portfolios/Portfolios";
+import Home from "./components/Home";
 function App() {
-  return <></>;
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={Home} />
+          <Route path="/skills" element={Skills} />
+          <Route path="/experiences" element={Experiences} />
+          <Route path="/educations" element={Educations} />
+          <Route path="/portfolios" element={Portfolios} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
