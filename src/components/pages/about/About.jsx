d14cb6a8cb4de22d6a9662f4df5f3507a7c93962
Amir Dhahri <amirdhahri01@gmail.com>
2024-10-18 23:32:24 +0100

    An update

--- src/components/pages/about/About.jsx
+++ src/components/pages/about/About.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <>
+      <div className="card">
+        <div className="card-content">
+          <h6>About me</h6>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default About;
