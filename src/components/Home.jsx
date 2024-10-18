942f53bb7b1fe60599a98e58052edb0f3fa952d3
Amir Dhahri <amirdhahri01@gmail.com>
2024-10-18 22:17:09 +0100

    An update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,12 @@
 import { Component } from "react";
 import Navbar from "./navbar/Navbar";
-import Profile from "./pages/Profile/Profile";
+import Profile from "./pages/profile/Profile";
 import Skills from "./pages/skills/Skills";
 import Experiences from "./pages/experiences/Experiences";
 import Educations from "./pages/educations/Educations";
 import Portfolios from "./pages/portfolios/Portfolios";
-import About from "./pages/About/About";
+import About from "./pages/about/About";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class Home extends Component {
   render() {
@@ -15,10 +16,10 @@ class Home extends Component {
           <Navbar />
           <div className="container">
             <div className="row">
-              <div className="col s12 m3">
+              <div className="col col-sm-12 col-md-3">
                 <Profile />
               </div>
-              <div className="col s12 m9">
+              <div className="col col-sm-12 col-md-9">
                 <About />
                 <Skills />
                 <Experiences />
