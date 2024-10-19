2157e6864ebd07c3022358ebf59437266af7a5fb
Amir Dhahri <amirdhahri01@gmail.com>
2024-10-19 13:11:55 +0100

    I've solved conflicts between bootstrap and materialise grid system and I've completed the experience component

--- src/components/pages/experiences/Experiences.jsx
+++ src/components/pages/experiences/Experiences.jsx
@@ -1,7 +1,59 @@
-import React from "react";
-
 const Experiences = () => {
-  return <div>Experiences</div>;
+  return (
+    <>
+      <div className="card">
+        <div className="card-content">
+          <h6>
+            <strong className="text-uppercase">Eperience</strong>
+          </h6>
+          <div className="row mt-3">
+            <div className="col s12 m6 l4 xl4">
+              <p className="teal year_exp white-text">
+                Jan
+                <strong>2020</strong> - Mar
+                <strong>2021</strong>
+              </p>
+            </div>
+            <div className="col s12 m6 l8 xl8">
+              <blockquote className="no-pad">
+                <h6 className="no-pad mt-bottom">
+                  <strong className="text-uppercase">Web developer</strong>
+                </h6>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam,
+                  earum doloribus dicta quod architecto, praesentium totam
+                  molestiae similique culpa repellat blanditiis nam facilis eius
+                  quaerat distinctio vitae, tenetur beatae repellendus?
+                </p>
+              </blockquote>
+            </div>
+          </div>
+          <div className="row mt-3">
+            <div className="col s12 m6 l4 xl4">
+              <p className="teal year_exp white-text">
+                Jan
+                <strong>2020</strong> - Mar
+                <strong>2021</strong>
+              </p>
+            </div>
+            <div className="col s12 m6 l8 xl8">
+              <blockquote className="no-pad">
+                <h6 className="no-pad mt-bottom">
+                  <strong className="text-uppercase">Web developer</strong>
+                </h6>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam,
+                  earum doloribus dicta quod architecto, praesentium totam
+                  molestiae similique culpa repellat blanditiis nam facilis eius
+                  quaerat distinctio vitae, tenetur beatae repellendus?
+                </p>
+              </blockquote>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Experiences;
